0eb83ebed63e00d35bcd15d0b9518b3648183669
klinkonsky <klinkonskydev@gmail.com>
2023-06-14 08:16:26 -0300

    feat: home loading

---
+++ src/app/loading.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import NextNProgress from 'nextjs-progressbar'
+
+import CardListSkeleton from 'components/CardList/skeleton'
+import Container from 'components/Container'
+import React from 'react'
+import HomeTemplate from 'templates/Home'
+
+export default function Loading() {
+  return (
+    <HomeTemplate>
+      <Container>
+        <CardListSkeleton />
+        <NextNProgress color="#FFA585" startPosition={0.3} stopDelayMs={200} height={8} showOnShallow={true} />
+      </Container>
+    </HomeTemplate>
+  )
+}
